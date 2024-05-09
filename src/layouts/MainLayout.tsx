d6cc18254c9c255972d839e25144efd1ef46bfd5
zhaobowen <bowen.zhao@chiefclouds.com>
2024-05-09 22:59:53 +0800

    fix: 修改路径的跳转

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,12 @@
 import react, { FC, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Avatar, Button, Drawer, Dropdown, Layout } from 'antd';
-import { UserOutlined, LogoutOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import styles from './MainLayout.module.less'
+import {
+  UserOutlined,
+  LogoutOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import styles from './MainLayout.module.less';
 import SideMenu from '../components/SideMenu';
 import { getUserId, getUserName, setUserLoginOut } from '../utils/user-info';
 import useNavPage from '../hooks/useNavPage';
@@ -15,39 +19,42 @@ import NavTitle from '../components/NavTitle';
 
 const { Header, Content } = Layout;
 
-const MainLayout:FC = (props) => {
+const MainLayout: FC = (props) => {
   const dispatch = useDispatch();
-  const [open, setOpen] = useState(false)
-  const isMobile = useMediaQuery("(max-width: 750px)");
-  const userName = ''//getUserName();
-  const userId = ''//getUserId();
+  const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery('(max-width: 750px)');
+  const userName = ''; //getUserName();
+  const userId = ''; //getUserId();
   const handleLogOutClick = () => {
-    setUserLoginOut()
-  }
-  const items =[
-  {
-    key: 2,
-    label: (
-      <div className={styles.menuItem}>
-        <LogoutOutlined />
-        <span className={styles.menuItemName} onClick={handleLogOutClick}>退出登录</span>
-      </div>
-    )
-  }]
+    setUserLoginOut();
+  };
+  const items = [
+    {
+      key: 2,
+      label: (
+        <div className={styles.menuItem}>
+          <LogoutOutlined />
+          <span className={styles.menuItemName} onClick={handleLogOutClick}>
+            退出登录
+          </span>
+        </div>
+      ),
+    },
+  ];
   const updateUserDetail = () => {
-    getUserDetail(userId).then((data:any) => {
-      dispatch(loginReducer({...data}))
-      setUserToken(data.token)
-    })
-  }
+    getUserDetail(userId).then((data: any) => {
+      dispatch(loginReducer({ ...data }));
+      setUserToken(data.token);
+    });
+  };
   const onClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
   useEffect(() => {
     // updateUserDetail();
-  }, [])
+  }, []);
   // 判断用户是否处于登录的状态
-  // useNavPage()
+  useNavPage();
   return (
     <Layout>
       <Header className={styles.header}>
@@ -55,7 +62,7 @@ const MainLayout:FC = (props) => {
           {/* <img src="./logo.png" alt="" /> */}
           <span className={styles.titleName}>无忧智能系统</span>
         </div>
-        {!isMobile ? 
+        {!isMobile ? (
           <Dropdown
             trigger={['click']}
             menu={{
@@ -63,24 +70,40 @@ const MainLayout:FC = (props) => {
             }}
           >
             <div>
-              <Avatar style={{cursor: 'pointer'}} size="large" icon={<UserOutlined />} />
-              <span style={{color: '#fff'}}>{userName}</span>
+              <Avatar
+                style={{ cursor: 'pointer' }}
+                size='large'
+                icon={<UserOutlined />}
+              />
+              <span style={{ color: '#fff' }}>{userName}</span>
             </div>
-          </Dropdown> : <Button icon={<MenuUnfoldOutlined />} onClick={() => {setOpen(true)}}>
-          </Button>
-        }
+          </Dropdown>
+        ) : (
+          <Button
+            icon={<MenuUnfoldOutlined />}
+            onClick={() => {
+              setOpen(true);
+            }}
+          ></Button>
+        )}
       </Header>
       <Content className={styles.main}>
-        { !isMobile && <SideMenu/>}
-        <div style={{flex:1, overflow: 'hidden'}}>
+        {!isMobile && <SideMenu />}
+        <div style={{ flex: 1, overflow: 'hidden' }}>
           <Outlet />
         </div>
       </Content>
-      <Drawer title="菜单" width='250' bodyStyle={{padding: 0}} onClose={onClose} open={open}>
-        <SideMenu/>
+      <Drawer
+        title='菜单'
+        width='250'
+        bodyStyle={{ padding: 0 }}
+        onClose={onClose}
+        open={open}
+      >
+        <SideMenu />
       </Drawer>
     </Layout>
-  )
-}
+  );
+};
 
 export default MainLayout;
